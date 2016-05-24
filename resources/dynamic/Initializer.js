358f324db3afd70365a74ed7317ddea1c73fb977
jeromed <jeromed@askia.com>
2016-05-24 18:08:35 +0200

    Added IEMobile
    Added IEMobile

--- resources/dynamic/Initializer.js
+++ resources/dynamic/Initializer.js
@@ -5,6 +5,6 @@
         javascriptSupport: {%= Browser.Support("javascript") %},
         controls: {%= On(CurrentADC.PropValue("controls") <> "true",0,1) %},
         autoplay: {%= On(CurrentADC.PropValue("autoplay") <> "true",0,1) %},
-        android: {%= On(Browser.UserAgent.IndexOf("Android") >= 1,1,0) %}
+        android: {%= On(Browser.UserAgent.IndexOf("Android") >= 1 or Browser.UserAgent.IndexOf("IEMobile") >= 1,1,0) %}
     });
 } ());
